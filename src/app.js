bb4f640eb15e48c0db1226dbd808540b58a75409
Deane Barker <deane@deanebarker.net>
2025-07-18 12:20:35 -0500

    Refactor request handling to use context management and improve logging

--- src/app.js
+++ src/app.js
@@ -3,27 +3,22 @@ import axios from "axios";
 import { resolveObject, resolveHtml } from "./resolver.js";
 import logger from "./logger.js";
 import config from "./config.js";
-import { request } from "./requestContext.js";
+import { addNote, getContext, initContext } from "./contextManager.js";
 
 const app = express();
 app.use(express.json()); // to support JSON-encoded bodies
 app.use(express.urlencoded({ extended: true })); // to support URL-encoded bodies
 
 // This is for simple HTML, no proxy
-if(config.getHtmlEndpoint())
-{
+if (config.getHtmlEndpoint()) {
   app.use(config.getHtmlEndpoint(), express.raw({ type: "*/*" }));
-  app.post(config.getHtmlEndpoint(), async (req, res) =>
-  {
-    request.set(req); // Set the request globally
+  app.post(config.getHtmlEndpoint(), async (req, res) => {
+    initContext(req);
 
-    try
-    {
-      const result  = await resolveHtml(req.body);
+    try {
+      const result = await resolveHtml(req.body);
       res.status(200).send(result);
-    }
-    catch (err)
-    {
+    } catch (err) {
       logger.error(`Error resolving HTML: ${err}`);
       res.status(500).send("Error resolving HTML");
     }
@@ -31,15 +26,12 @@ if(config.getHtmlEndpoint())
 }
 
 // This is for a proxied API call
-app.use(async (req, res) =>
-{
-  request.set(req); // Set the request globally
+app.use(async (req, res) => {
+  initContext(req);
 
-  res.locals.log = [];
   logger.info(`Received request: ${req.method} ${req.originalUrl}`);
 
-  try
-  {
+  try {
     // Transform the existing request to point at the origin
     let newConfig = config.getModifiedRequest(req);
 
@@ -53,22 +45,20 @@ app.use(async (req, res) =>
     // Resolve the response data
     const tsStartResolution = Date.now();
     await resolveObject(response.data);
-    res.locals.log.push(
-      `Resolution time: ${Date.now() - tsStartResolution} ms`
-    );
+    addNote(`Resolution time: ${Date.now() - tsStartResolution} ms`);
 
     // Send resolved request back
-    response.data._log = res.locals.log;
+    response.data._log = {
+      notes: getContext().notes,
+      errors: getContext().errors,
+    };
     res.status(response.status).set(response.headers).send(response.data);
-  }
-  catch (err)
-  {
+  } catch (err) {
     logger.error(`Error processing request: ${err.message}`);
     res.status(500).send("Proxy error");
   }
 });
 
-app.listen(config.getPort(), () =>
-{
+app.listen(config.getPort(), () => {
   console.log(`Proxy server running on http://localhost:${config.getPort()}`);
 });
